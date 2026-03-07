77b2198502a2a21cf02c9a1de805ed4491b50c35
weneed1t <sladost.nikituncik@gmail.com>
2026-03-07 21:48:45 +0000

    im so sleppy

--- src/t5_connect_data.rs
+++ src/t5_connect_data.rs
@@ -1,7 +1,7 @@
 use crate::t1queue_tcpudp::recv_queue::{WSQueueErr, WSRecvQueueCtrs, WSUdpLike, WSWaitQueue};
 use crate::t3poc_files::WSFileSplitter;
 use crate::t4algo_param::WsConnectParam;
-use crate::wt1_types::{EncWis, MyRole /* , Cfcser, Noncer */, PackErr, Thrasher};
+use crate::wt1_types::{EncWis, MyRole /* , Cfcser, Noncer ,WTypeErr */, PackErr, Thrasher};
 pub struct Ids {
     pub id_sender: u64,
     pub id_receiver: u64,
@@ -23,19 +23,19 @@ pub struct WsConnection<
 > {
     file_proc: WSFileSplitter,
     udp_queue: WSUdpLike<Tudp>,
-    wait_queue: WSWaitQueue<Twait, f32>,
+    wait_queue: WSWaitQueue<Twait, f64>,
     fback_queue: WSRecvQueueCtrs,
     ctr_data: u64,
     ctr_fback: u64,
     network_stability: f64,
     network_latency: f64,
-    enrypt: Tencrypt,
+    encrypt: Tencrypt,
     connect_param: WsConnectParam,
     enrypaaa: bool,
     is_active: bool,
     //nonce_gener: Option<Tnoncer>,
     //cfc_gener: Option<TCfcser>,
-    nonce_gener: Option<TThrasher>,
+    user_field: Option<TThrasher>,
     measurement_window_latency: f64,
     my_role: MyRole,
     my_identified: Identified,
@@ -69,17 +69,20 @@ impl<
                 connect_param
                     .pack_topology()
                     .counter_slice()
-                    .ok_or(WSQueueErr::Critical(""))?
+                    .ok_or(WSQueueErr::Critical(
+                        ".pack_topology().counter_slice() is empty, but the algorithm requires \
+                         the explicit presence of a counter!!!",
+                    ))?
                     .2,
                 connect_param.maximum_length_fback_queue_packages(),
                 connect_param.mtu(),
             )
-            .unwrap(),
+            .map_err(WSQueueErr::Critical)?,
             ctr_data: 0,
             ctr_fback: 1,
             network_stability: 0.0,
             network_latency: 0.0,
-            enrypt: Tencrypt::new(default_enc_key).map_err(WSQueueErr::Critical)?,
+            encrypt: Tencrypt::new(default_enc_key).map_err(WSQueueErr::Critical)?,
             connect_param: connect_param.clone(),
             enrypaaa: true,
             is_active: true,
@@ -108,7 +111,7 @@ impl<
                 None
             },
             */
-            nonce_gener: if connect_param
+            user_field: if connect_param
                 .pack_topology()
                 .trash_content_slice()
                 .is_some()
@@ -130,7 +133,7 @@ impl<
         })
     }
 
-    fn addtwo(&self, num: &mut u64) -> Result<(), PackErr> {
+    fn add_two(&self, num: &mut u64) -> Result<(), PackErr> {
         *num = num.checked_add(2).ok_or(PackErr::UndefinedErr(
             "The capacity limit of the main counter u64 has been reached, so it is no longer \
              possible to send new messages over this connection. The connection must be closed!",
